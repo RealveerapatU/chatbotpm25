afb3b83e7e37a891b55c1e705c63566e6691222d
veerapat <veeraput.kb09834@gmail.com>
2025-06-07 13:17:12 +0700

    update default port from 3000 to 3002

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const template = require("./template");
 const util = require("./util");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
